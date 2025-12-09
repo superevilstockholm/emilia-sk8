e3edf45b8ca04b19cddb80c08f8bab647774fff4
superevilstockholm <balilombok11@gmail.com>
2025-12-09 22:14:21 +0700

    Add google fonts Inter and Poppins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   pages: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/google-fonts",
+  ],
+  googleFonts: {
+    families: {
+      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      Poppins: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    display: "swap",
+    preload: true,
+    preconnect: true,
+  }
 });
